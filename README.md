1252e5cb35b61328f21e265e3776e27a37d88928
dandtorre <dan.delatorre@linux.com>
2021-10-14 20:06:53 +0800

    =users

--- README.md
+++
@@ -1,6 +0,0 @@
-# puppetlabs-code
-puppet-config
-
-email: dan.delatorre@linux.com
-
-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
--- modules/dt_users/manifests/master.pp
+++ modules/dt_users/manifests/master.pp
@@ -27,12 +27,11 @@ class dt_users::master
 
     ssh_authorized_key 
     { 'masters@jmp.sg1.delatorre.network':
-       
-       name => "masters@jmp.sg1.delatorre.network",
-       ensure => absent,
-       key => "AAAAB3NzaC1yc2EAAAADAQABAAABAQCekShkWBGexAeCgAjAcgnoQkFyuFwrrHvIsztHN4MGyQDa1s2PQrsoxnsH1Ude83MOXvaKHwEJA6xxVXpv+v6Jov81j6po2riDC9Vs+n+1+I2i3Cg0MENFNUZ3b5MFWOnZRMbbR2uTt7Av81N9vxyogcKMAeur9WeOs9BUs08sXGekYI5ePkUORqthe9t5/QA39tiFQTqMd5SL7gRvsdhZk745gHcpBirDaebatn6kIIq4OQamlB7yumlVEiCHNYzZmvl2QeU14yKp+307nBwdDbJglr5+ZP5OBeZSNrvXaQuYblAcu5ldFI9Tf3eM3rr1zWMkYkXJFoPLkDdQ94iT",
-       type => ssh-rsa,
-       user => masters,
+     
+    	ensure => present,
+  	user   => 'masters',
+  	type   => 'ssh-rsa',
+  	key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCekShkWBGexAeCgAjAcgnoQkFyuFwrrHvIsztHN4MGyQDa1s2PQrsoxnsH1Ude83MOXvaKHwEJA6xxVXpv+v6Jov81j6po2riDC9Vs+n+1+I2i3Cg0MENFNUZ3b5MFWOnZRMbbR2uTt7Av81N9vxyogcKMAeur9WeOs9BUs08sXGekYI5ePkUORqthe9t5/QA39tiFQTqMd5SL7gRvsdhZk745gHcpBirDaebatn6kIIq4OQamlB7yumlVEiCHNYzZmvl2QeU14yKp+307nBwdDbJglr5+ZP5OBeZSNrvXaQuYblAcu5ldFI9Tf3eM3rr1zWMkYkXJFoPLkDdQ94iT',  
     }
 
 }
